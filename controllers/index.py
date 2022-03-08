1564b99ffdb3d41630f4ffc90b687a4bb18272f8
gl@bulme.at <gl@bulme.at>
2022-03-08 09:32:13 +0100

    Added Products Table

--- controllers/index.py
+++ controllers/index.py
@@ -8,11 +8,5 @@ index_blueprint = Blueprint('index_blueprint', __name__)
 
 @index_blueprint.route("/")
 def index():
-    #workaround für sesssion Autocomplete
-    session : sqlalchemy.orm.scoping.scoped_session = db.session
-    
-    #alle customers laden
-    customers = session.query(Customer).all()
-    print(customers)
 
     return render_template("index.html")
--- controllers/products.py
+++ controllers/products.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask.templating import render_template
 from flask import Blueprint
 import sqlalchemy
-from model.models import db,Customer
+from model.models import Product, db,Customer
 
 products_blueprint = Blueprint('products_blueprint', __name__)
 
 @products_blueprint.route("/products")
 def products():
+    #workaround für sesssion Autocomplete
+    session : sqlalchemy.orm.scoping.scoped_session = db.session
+    
+    #alle products laden
+    products = session.query(Product).all()
 
-    return render_template("products.html")
+    return render_template("products.html", products = products)
